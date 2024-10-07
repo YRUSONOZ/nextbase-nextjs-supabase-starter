e0d8779248c80d5e07d51f174b0a0072c3a47f97
YRUSONOZ <74988686+YRUSONOZ@users.noreply.github.com>
2024-10-06 23:55:06 -0700

    Update page.tsx

--- src/app/(dynamic-pages)/(main-pages)/page.tsx
+++ src/app/(dynamic-pages)/(main-pages)/page.tsx
@@ -15,18 +15,17 @@ export default function Component() {
               alt="Hero"
               className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
               height="550"
-              src="https://generated.vusercontent.net/placeholder.svg"
+              src="https://imageio.forbes.com/specials-images/imageserve/65258a15bb0f1e44bfaf1887/Social-media-Social--media--Marketing--Thailand--Social-Media--Engagement--Post--/960x0.jpg?format=jpg&width=1440"
               width="550"
             />
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                  The complete platform for building the Web
+                  The complete platform for building a social presence.
                 </h1>
                 <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Give your team the toolkit to stop configuring and start
-                  innovating. Securely build, deploy, and scale the best web
-                  experiences.
+                  Give your team the toolkit to grow your online presence.
+                  Securely create, connect, schedule, and post! Grow quicker with less effort!
                 </p>
               </div>
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
