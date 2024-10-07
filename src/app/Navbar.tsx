5f7fd6289878ffe97bd24fb4a5a3c923a5a19bf1
YRUSONOZ <74988686+YRUSONOZ@users.noreply.github.com>
2024-10-06 23:37:38 -0700

    Update Navbar.tsx

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -102,7 +102,7 @@ export const ExternalNavigation = () => {
         </Link>
         <Link
           className="text-sm hidden lg:block font-medium hover:underline underline-offset-4"
-          href="#"
+          href="www.google.com"
         >
           About
         </Link>
